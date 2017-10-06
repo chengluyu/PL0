b377925b46f4957bdd3d8bfa987d5f77786a12a8
Synthetic Dream <chengluyu@users.noreply.github.com>
2017-10-06 17:00:27 +0800

    Refactor VM (the simulator).

--- python3/vm.py
+++ python3/vm.py
@@ -1,4 +1,4 @@
-from codegen import Opt, Opcode
+from codegen import Opt, Opcode, prettify_instruction
 from lexer import Lexer
 from parse import Parser
 
@@ -15,111 +15,95 @@ BIFUNCTORS = {
     Opt.GEQ: (lambda x, y: 1 if x >= y else 0)
 }
 
-def fill(n):
-    result = list()
-    for i in range(n):
-        result.append(0)
-    return result
-
 class StackFrame:
     def __init__(self, return_address, enclosing_frame, parent_frame=None):
         self.parent_frame = parent_frame
         self.enclosing_frame = enclosing_frame
         self.return_address = return_address
         self.locals = None
-        self.eval_stack = list()
+        self.stack = list()
+
     def allocate_locals(self, local_var_count):
-        self.locals = fill(local_var_count)
+        self.locals = [0 for i in range(local_var_count)]
+        
+    def resolve_frame(self, dist):
+        frame = self
+        while dist > 0:
+            dist -= 1
+            frame = frame.parent_frame
+        return frame
 
 class VM:
-    def __init__(self):
-        pass
-    def execute(self, code, entry=0):
-        top = StackFrame(len(code), None)
-        program_counter = entry
-        # callbacks for each opcode
-        def get_target_frame(level):
-            nonlocal top
-            target_frame = top
-            while level > 0:
-                level -= 1
-                target_frame = target_frame.parent_frame
-            return target_frame
-        def on_lit(_, value):
-            nonlocal top
-            top.eval_stack.append(value)
-        def on_lod(level, index):
-            nonlocal top
-            target_frame = get_target_frame(level)
-            value = target_frame.locals[index]
-            top.eval_stack.append(value)
-        def on_sto(level, index):
-            nonlocal top
-            target_frame = get_target_frame(level)
-            assert len(top.eval_stack) > 0
-            value = top.eval_stack.pop()
-            target_frame.locals[index] = value
-        def on_cal(level, entry):
-            nonlocal program_counter
-            nonlocal top
-            # print('Jump to procedure at %d' % entry)
-            frame = top
-            while level > 0 and frame is not None:
-                level -= 1
-                frame = frame.parent_frame
-            top = StackFrame(program_counter, top, frame)
-            program_counter = entry
-        def on_int(_, size):
-            nonlocal top
-            assert size >= 3;
-            top.allocate_locals(size - 3)
-        def on_jmp(_, target):
-            nonlocal program_counter
-            # print('Unconditionally jump to %d' % target)
-            assert target < len(code);
-            program_counter = target
-        def on_jpc(_, target):
-            nonlocal program_counter
-            nonlocal top
-            assert len(top.eval_stack) > 0
-            value = top.eval_stack.pop()
-            if value == 0:
-                # print('Conditionally jump to %d' % target)
-                program_counter = target
-        def on_opr(_, op):
-            nonlocal program_counter
-            nonlocal top
-            if op in BIFUNCTORS:
-                assert len(top.eval_stack) >= 2
-                rhs = top.eval_stack.pop()
-                lhs = top.eval_stack.pop()
-                top.eval_stack.append(BIFUNCTORS[op](lhs, rhs))
-            elif op == Opt.READ:
-                value = int(input('--> '))
-                top.eval_stack.append(value)
-            elif op == Opt.WRITE:
-                assert len(top.eval_stack) > 0
-                arg = top.eval_stack.pop()
-                print(arg)
-            elif op == Opt.RET:
-                program_counter = top.return_address
-                top = top.enclosing_frame
-            else:
-                raise Exception('invalid operator')            
-        # dict for callbacks
-        cbs = {
-            Opcode.LIT: on_lit,
-            Opcode.LOD: on_lod,
-            Opcode.STO: on_sto,
-            Opcode.CAL: on_cal,
-            Opcode.INT: on_int,
-            Opcode.JMP: on_jmp,
-            Opcode.JPC: on_jpc,
-            Opcode.OPR: on_opr
+    def __init__(self, code, entry=0):
+        self.code = code
+        self.top = StackFrame(len(code), None)
+        self.program_counter = entry
+        self.handlers = {
+            Opcode.LIT: self.on_lit,
+            Opcode.LOD: self.on_lod,
+            Opcode.STO: self.on_sto,
+            Opcode.CAL: self.on_cal,
+            Opcode.INT: self.on_int,
+            Opcode.JMP: self.on_jmp,
+            Opcode.JPC: self.on_jpc,
+            Opcode.OPR: self.on_opr
         }
-        # do iteration on instructions
-        while program_counter < len(code):
-            print('Execute instruction at %d' % program_counter)
-            (opcode, level, address, _) = code[program_counter]
-            program_counter += 1
-            cbs[opcode](level, address)
+
+    def on_lit(self, _, value):
+        self.top.stack.append(value)
+
+    def on_lod(self, level, index):
+        target_frame = self.top.resolve_frame(level)
+        self.top.stack.append(target_frame.locals[index])
+
+    def on_sto(self, level, index):
+        target_frame = self.top.resolve_frame(level)
+        assert len(self.top.stack) > 0
+        target_frame.locals[index] = self.top.stack.pop()
+
+    def on_cal(self, level, entry):
+        frame = self.top.resolve_frame(level)
+        self.top = StackFrame(self.program_counter, self.top, frame)
+        self.program_counter = entry
+
+    def on_int(self, _, size):
+        assert size >= 3;
+        self.top.allocate_locals(size - 3)
+
+    def on_jmp(self, _, target):
+        assert target < len(self.code);
+        self.program_counter = target
+
+    def on_jpc(self, _, target):
+        assert len(self.top.stack) > 0
+        if self.top.stack.pop() == 0:
+            self.program_counter = target
+
+    def on_opr(self, _, op):
+        if op in BIFUNCTORS:
+            assert len(self.top.stack) >= 2
+            rhs = self.top.stack.pop()
+            lhs = self.top.stack.pop()
+            res = BIFUNCTORS[op](lhs, rhs)
+            self.top.stack.append(res)
+        elif op == Opt.READ:
+            value = int(input('--> '))
+            self.top.stack.append(value)
+        elif op == Opt.WRITE:
+            assert len(self.top.stack) > 0
+            arg = self.top.stack.pop()
+            print(arg)
+        elif op == Opt.RET:
+            self.program_counter = self.top.return_address
+            self.top = self.top.enclosing_frame
+        else:
+            raise Exception('invalid operator %s' % op)
+
+    def execute(self, show_trace=False):
+        while self.program_counter < len(self.code):
+            ins = self.code[self.program_counter]
+            (opcode, level, address, comment) = ins
+            if show_trace:
+                print('%d\t' % self.program_counter, prettify_instruction(ins))
+            self.program_counter += 1
+            self.handlers[opcode](level, address)
