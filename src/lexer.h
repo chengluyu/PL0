f2a809bf857ffd900eb6ddcb2ce9d2f5f8b90004
chengluyu <chengluyu@live.cn>
2017-10-10 15:45:37 +0800

    Extract exception class into a new file.

--- src/lexer.h
+++ src/lexer.h
@@ -6,44 +6,9 @@
 #include <stdexcept>
 
 #include "token.h"
+#include "util.h"
 
 namespace pl0 {
-
-#ifdef _MSC_VER
-namespace polyfill {
-
-template <typename T, typename... Args>
-void fold_write_stream(std::ostringstream &oss, T value, Args... args) {
-	oss << value;
-	fold_write_stream(oss, args...);
-}
-
-template <typename T>
-void fold_write_stream(std::ostringstream &oss, T value) {
-	oss << value;
-}
-
-}
-#endif
-
-class lexical_error {
-	std::string message_;
-public:
-	template <typename... Args>
-	lexical_error(Args... args) {
-		std::ostringstream oss;
-#ifdef _MSC_VER
-		// Visual Studio 2017 does not support fold expression now.
-		// We need to make a polyfill.
-		polyfill::fold_write_stream(oss, args...);
-#else
-		oss << ... << args;
-#endif
-		message_ = oss.str();
-	}
-
-	const std::string &what() const { return message_; }
-};
 
 class lexer {
 	std::istream &input_stream_;
@@ -67,7 +32,7 @@ public:
 
 	token expect(token_type tk) {
 		if (peek_.first == tk) return next();
-		throw lexical_error("expect ", *tk, " instead of ", *peek_.first);
+		throw general_error("expect ", *tk, " instead of ", *peek_.first);
 	}
 
 	std::optional<token> match(token_type tk) {
---
+++ src/util.h
@@ -0,0 +1,44 @@
+#ifndef PL_ZERO_UTIL_H
+#define PL_ZERO_UTIL_H
+
+namespace pl0 {
+
+#ifdef _MSC_VER
+namespace polyfill {
+
+template <typename T, typename... Args>
+void fold_write_stream(std::ostringstream &oss, T value, Args... args) {
+	oss << value;
+	fold_write_stream(oss, args...);
+}
+
+template <typename T>
+void fold_write_stream(std::ostringstream &oss, T value) {
+	oss << value;
+}
+
+}
+#endif
+
+class general_error {
+	std::string message_;
+public:
+	template <typename... Args>
+	general_error(Args... args) {
+		std::ostringstream oss;
+#ifdef _MSC_VER
+		// Visual Studio 2017 does not support fold expression now.
+		// We need to make a polyfill.
+		polyfill::fold_write_stream(oss, args...);
+#else
+		oss << ... << args;
+#endif
+		message_ = oss.str();
+	}
+
+	const std::string &what() const { return message_; }
+};
+
+}
+
+#endif
