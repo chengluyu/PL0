e2cdb6744bea620dd11841f2d996611fe821deeb
chengluyu <chengluyu@live.cn>
2017-10-27 01:29:58 +0800

    Add support of showing the location of syntax error.

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -12,54 +12,63 @@ inline bool is_identifier_part(int ch) {
     return isalnum(ch) || ch == '_';
 }
 
+int lexer::get() {
+    column_++;
+    return input_stream_.get();;
+}
+
+token lexer::select(char cond, token conseq, token altern) {
+    if (input_stream_.peek() == cond) {
+        get();
+        return conseq;
+    } else {
+        return altern;
+    }
+}
+
 void lexer::advance() {
-    // clear buffer
-    literal_buffer_.clear();
+    // check if already end of source or meet illegal chars
+    if (peek_ == token::EOS || peek_ == token::ILLEGAL) {
+        return;
+    }
     // ignore whitespaces
     while (isspace(input_stream_.peek())) {
-        input_stream_.ignore();
+        if (get() == '\n') {
+            line_++;
+            column_ = 1;
+        }
     }
     // check if input stream has ended
     if (input_stream_.peek() == std::char_traits<char>::eof()) {
         peek_ = token::EOS;
         return;
     }
+    // clear literal buffer
+    literal_buffer_.clear();
     // identifier or keyword
     if (is_identifier_start(input_stream_.peek())) {
         do {
-            literal_buffer_.push_back(input_stream_.get());
+            literal_buffer_.push_back(get());
         } while (is_identifier_part(input_stream_.peek()));
         auto iter = keyword_map.find(literal_buffer_);
-        if (iter == keyword_map.end()) {
-            peek_ = token::IDENTIFIER;
-        } else {
-            peek_ = iter->second;
-        }
+        peek_ = iter == keyword_map.end() ? token::IDENTIFIER : iter->second;
         return;
     }
     // number
     if (isdigit(input_stream_.peek())) {
         do {
-            literal_buffer_.push_back(input_stream_.get());
+            literal_buffer_.push_back(get());
         } while (isdigit(input_stream_.peek()));
         peek_ = token::NUMBER;
         return;
     }
-    // punctuator or operator
-    switch (input_stream_.get()) {
+    // punctuator, operator or unrecognized character
+    switch (get()) {
     case '+': peek_ = token::ADD; break;
     case '-': peek_ = token::SUB; break;
     case '*': peek_ = token::MUL; break;
     case '/': peek_ = token::DIV; break;
-    case ':':
-        if (input_stream_.peek() == '=') {
-            peek_ = token::ASSIGN;
-            input_stream_.ignore();
-        } else {
-            peek_ = token::ILLEGAL;
-            input_stream_.unget();
-        }
-        break;
+    case ':': peek_ = select('=', token::ASSIGN, token::ILLEGAL); break;
     case '(': peek_ = token::LPAREN; break;
     case ')': peek_ = token::RPAREN; break;
     case ';': peek_ = token::SEMICOLON; break;
@@ -67,26 +76,9 @@ void lexer::advance() {
     case ',': peek_ = token::COMMA; break;
     case '=': peek_ = token::EQ; break;
     case '#': peek_ = token::NEQ; break;
-    case '<':
-        if (input_stream_.peek() == '=') {
-            peek_ = token::LEQ;
-            input_stream_.ignore();
-        } else {
-            peek_ = token::LE;
-        }
-        break;
-    case '>':
-        if (input_stream_.peek() == '=') {
-            peek_ = token::GEQ;
-            input_stream_.ignore();
-        } else {
-            peek_ = token::GE;
-        }
-        break;
-    default:
-        peek_ = token::ILLEGAL;
-        input_stream_.unget();
-        break;
+    case '<': peek_ = select('=', token::LEQ, token::LE); break;
+    case '>': peek_ = select('=', token::GEQ, token::GE); break;
+    default:  peek_ = token::ILLEGAL; break;
     }
 }
 
